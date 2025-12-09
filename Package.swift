b2c7e9e4f50b65eae62d6ed3343e990093a34877
Fredy Leon <fredy.leon@ua.la.com>
2025-12-09 16:03:29 +0100

    add utils package

--- Package.swift
+++ Package.swift
@@ -5,18 +5,38 @@ import PackageDescription
 
 let package = Package(
     name: "FHKCore",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FHKCore",
             targets: ["FHKCore"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git",
+            .upToNextMajor(from: "12.6.0")),
+        
+        .package(url: "https://github.com/leonodev/fintechKids-modulo-utils-ios.git",
+                 exact: "1.0.2")
+        ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FHKCore"
+            name: "FHKCore",
+            dependencies: [
+                // Modules Firebase
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
+                
+                // Modules Utils
+                .product(name: "FHKUtils", package: "fintechKids-modulo-utils-ios")
+            ]
         ),
         .testTarget(
             name: "FHKCoreTests",
