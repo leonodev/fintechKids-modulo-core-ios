552b7ad8242e945b76c5f97e3760e193f9d348dc
Fredy Leon <fredy.leon@ua.la.com>
2026-01-08 10:10:52 +0100

    add state views

--- Sources/FHKCore/ViewModel/FHKCore+ViewModel.swift
+++ Sources/FHKCore/ViewModel/FHKCore+ViewModel.swift
@@ -15,3 +15,34 @@ public extension FHKCore {
         func action(_ action: Action) async
     }
 }
+
+
+public extension FHKCore {
+    public enum State<T> {
+        case none
+        case loading
+        case loaded(T?)
+        case error(Error)
+        
+        public var data: T? {
+            if case let .loaded(value) = self {
+                return value
+            }
+            return nil
+        }
+          
+        public var isLoading: Bool {
+            if case .loading = self {
+                return true
+            }
+            return false
+        }
+        
+        public var isLoaded: Bool {
+            if case .loaded = self {
+                return true
+            }
+            return false
+        }
+    }
+}
