1f66c8d11c36a712211b97ff4cfa71b11937688d
Fredy Leon <fredy.leon@ua.la.com>
2026-02-15 18:42:22 +0100

    add extra info atributes

--- Sources/FHKCore/Crashlytics/LogAttributes.swift
+++ Sources/FHKCore/Crashlytics/LogAttributes.swift
@@ -34,18 +34,27 @@ public struct LogAttributes: LogAttributesProtocol {
     public var userID: String?
     public var action: String?
     public var feature: FHKFeature?
+    public var extraInfo: [String: Any]?
     
     // Default initializer
     public init(
         platform: Platform? = nil,
         userID: String? = nil,
         action: String? = nil,
-        feature: FHKFeature? = nil
+        feature: FHKFeature? = nil,
+        extraInfo: [String: Any]? = nil
     ) {
         self.platform = platform
         self.userID = userID
         self.action = action
         self.feature = feature
+        self.extraInfo = extraInfo
+    }
+    
+    public static func == (lhs: LogAttributes, rhs: LogAttributes) -> Bool {
+        return lhs.userID == rhs.userID &&
+        lhs.action == rhs.action &&
+        lhs.platform == rhs.platform
     }
     
     /// Converts the struct into a dictionary for send.
@@ -54,6 +63,12 @@ public struct LogAttributes: LogAttributesProtocol {
         if let userID = userID { dict["user_id"] = userID }
         if let action = action { dict["action"] = action }
         if let feature = feature { dict["feature"] = feature.name }
+        
+        if let extraInfo = extraInfo {
+            // merge avoids collisions or allows the extra to overwrite values ​​if you want
+            dict.merge(extraInfo) { (current, _) in current }
+        }
+        
         return dict
     }
 }
