5f7e1afc993fc2b4b9008671ed79b4ee230a664d
Fredy Leon <fredy.leon@ua.la.com>
2026-02-28 22:03:03 +0100

    remove firebase lib

--- Package.swift
+++ Package.swift
@@ -15,9 +15,6 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git",
-            .upToNextMajor(from: "12.6.0")),
-        
         .package(url: "https://github.com/leonodev/fintechKids-modulo-domain-ios.git",
                 branch: "main")
         ],
@@ -25,15 +22,6 @@ let package = Package(
         .target(
             name: "FHKCore",
             dependencies: [
-                // Modules Firebase
-                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
-                
                 // Modules FHK
                 .product(name: "FHKDomain", package: "fintechKids-modulo-domain-ios")
             ],
