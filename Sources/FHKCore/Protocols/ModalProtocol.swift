ee2395c3cf41b40f6debfec5132facdc1426d99a
Fredy Leon <fredy.leon@ua.la.com>
2026-03-07 13:20:51 +0100

    remove protocols

--- Sources/FHKCore/Protocols/ModalProtocol.swift
+++ Sources/FHKCore/Protocols/ModalProtocol.swift
@@ -5,11 +5,11 @@
 //  Created by Fredy Leon on 2/3/26.
 //
 
-import SwiftUI
-
-public protocol FHKModalProtocol: AnyObject, Sendable {
-    var isPresented: Bool { get set }
-    var content: AnyView? { get set }
-    func show<V: View>(@ViewBuilder _ content: () -> V)
-    func dismiss()
-}
+//import SwiftUI
+//
+//public protocol FHKModalProtocol: AnyObject, Sendable {
+//    var isPresented: Bool { get set }
+//    var content: AnyView? { get set }
+//    func show<V: View>(@ViewBuilder _ content: () -> V)
+//    func dismiss()
+//}
